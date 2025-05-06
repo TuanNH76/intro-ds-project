9f4de9b0b50e711206c17b5c683cff3faf4ae67b
luvu4ever <dungfixbug@gmail.com>
2025-05-06 17:18:24 +0700

    fix init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,15 @@
-from .random_forest import RandomForest
-from .sarimax import Sarimax
-from .orbit import Orbit
+from .random_forest import MyRandomForest
+from .sarimax import MySARIMAX
 from .LSTM import MyLSTM
 from .GRU import MyGRU
 from .arima import MyARIMA
-from .prophet import MyProphet
-from .xgboost import MyXGboost
-from .neural_prophet import Neural_Prophet
-
+from .xgboost import MyXGBoost
 
 MODELS = {
-            'random_forest': RandomForest,
-          'sarimax': Sarimax,
-          'orbit': Orbit,
-          'lstm': MyLSTM,
-          'gru': MyGRU,
-          'arima': MyARIMA,
-          'prophet': MyProphet,
-          'xgboost': MyXGboost,
-          'neural_prophet': Neural_Prophet
-          }
+    'random_forest': MyRandomForest,
+    'sarimax': MySARIMAX,
+    'lstm': MyLSTM,
+    'gru': MyGRU,
+    'arima': MyARIMA,
+    'xgboost': MyXGBoost,
+}
